fb04f6c5024185f67004394b1d1882c1b7e61e8a
Stanley Francess <stanleyjanefrancess@gmail.com>
2022-11-20 12:42:37 -0800

    Header Section
    Header background image, navigation and header text was added with some js typewriter effect.
    
    20 - 11-2022

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".orange-text", {
+    strings: ["Stanley Francess", "A Web Developer", "A UI/UX Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true,
+    backDelay: 3000,
+})
