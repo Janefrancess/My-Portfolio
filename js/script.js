6c56b0c8406dc6ae56e09f68d531fe2375d888c2
Stanley Francess <stanleyjanefrancess@gmail.com>
2022-12-27 05:36:12 -0800

    Scrollable Navigation
    Scrollable Navigation activated and contact details attached.
    28-12-2022

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
 var typed = new Typed(".orange-text", {
-    strings: ["Stanley Francess", "A Web Developer", "A UI/UX Designer"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    loop: true,
-    backDelay: 3000,
-})
+strings: ["Stanley Francess", "A Web Developer", "A UI/UX Designer"],
+typeSpeed: 100,
+backSpeed: 100,
+loop: true,
+backDelay: 3000,
+});
+
+var navbar = document.getElementById("navbar");
+
+window.onscroll = function(){
+if(window.pageYOffset >= navbar.offsetTop){
+navbar.classList.add("sticky");
+}else{
+navbar.classList.remove("sticky");
+}
+}
